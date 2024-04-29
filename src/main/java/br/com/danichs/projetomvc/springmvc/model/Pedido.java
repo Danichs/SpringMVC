529044576b233b738010432dbe61312f9865cd65
Joao Victor Cardozo <joao.cardozo@asaas.com.br>
2024-04-29 13:30:16 -0300

    Finalização da estilização das páginas, criação do formulário de cadastramento de produtos, implementações de rotas e requisições.

--- src/main/java/br/com/danichs/projetomvc/springmvc/model/Pedido.java
+++ src/main/java/br/com/danichs/projetomvc/springmvc/model/Pedido.java
@@ -4,6 +4,8 @@ import java.math.BigDecimal;
 import java.time.LocalDate;
 
 import jakarta.persistence.Entity;
+import jakarta.persistence.EnumType;
+import jakarta.persistence.Enumerated;
 import jakarta.persistence.GeneratedValue;
 import jakarta.persistence.GenerationType;
 import jakarta.persistence.Id;
@@ -23,6 +25,9 @@ public class Pedido {
     private String urlProduto;
     private String urlImagem;
     private String descricao;
+
+    @Enumerated(EnumType.STRING)
+    private StatusPedido status;
     
     public String getNomeProduto() {
         return nomeProduto;
@@ -72,5 +77,12 @@ public class Pedido {
         this.descricao = descricao;
     }
 
+    public StatusPedido getStatus(){
+        return status;
+    }
+
+    public void setStatus(StatusPedido status){
+        this.status = status;
+    }
 
 }
---
+++ src/main/java/br/com/danichs/projetomvc/springmvc/model/StatusPedido.java
@@ -0,0 +1,5 @@
+package br.com.danichs.projetomvc.springmvc.model;
+
+public enum StatusPedido {
+    AGUARDANDO, APROVADO, ENTREGUE;
+}
